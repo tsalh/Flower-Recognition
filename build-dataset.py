e246694b2e97c74ce44de548cdf0f01f58e33d7e
Tarun Salh <tsalh2019@gmail.com>
2019-02-17 16:33:20 -0800

    Updated building dataset script

--- build-dataset.py
+++ build-dataset.py
@@ -49,9 +49,35 @@ def build_dataset( data_file ):
             zip_file.extract( dict[label][i], os.path.join("", "test"))
             i += 1
 
+# Function checks if the test and train directories exist, if they do delete
+# those directories and its contents, then recreate the test and train directory
+# again
+def setup_workspace( path ):
+
+    # If test or train path already exists go delete the test or train directory
+    # and all the contents inside the directory
+    # Assumptions made:
+    # 1. After the train or test folder there are 5 subfolders of the labels
+    # 2. Inside each subfolder are regular files ( images in the case for this
+    #     project )
+    if os.path.exists(path):
+        subfolders = os.listdir(path)
+        # Subfolder directories
+        for subfolder in subfolders:
+            subfolder_name = os.path.join(path, subfolder)
+            files = os.listdir( subfolder_name )
+            # For files in the subfolder
+            for file in files:
+                os.remove(os.path.join(subfolder_name, file))
+            os.removedirs(subfolder_name)
+    os.mkdir(path)
+
 ###############################################################################
 ## Program Entry Point ########################################################
 ###############################################################################
 if __name__ == '__main__':
 
+    # setup workspace in train and test path
+    setup_workspace(TRAIN_PATH)
+    setup_workspace(TEST_PATH)
     build_dataset( "flowers.zip" )
